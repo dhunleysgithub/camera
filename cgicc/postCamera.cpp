165bd96c4e01833c3b16fa056384d9945a47582d
dhunleysgithub <dwighthunley0@gmail.com>
2016-06-23 15:55:15 -0700

    Update postCamera.cpp

--- cgicc/postCamera.cpp
+++ cgicc/postCamera.cpp
@@ -37,7 +37,28 @@ ofstream myfile;
    Cgicc form;                                     // the CGI form object
    string pwr_cmd, command, exposureCount, exposuresRequested, exposuresReset, expsReset;
    bool isReset = form.queryCheckbox("expsReset");   // get the state of the status checkbox
-myfile.open ("example.html");
+   
+          if (isReset){
+
+  cout << "<div> Resetting the exposure count to 0  "  << "</div>";
+  ofstream myfile2 (ss.str().c_str());
+  if (myfile2.is_open())
+  {
+    myfile2 << 0;
+    myfile2.close();
+  }
+  else {
+     //cout << "Unable to open file";
+     cout << "<div> Unable to reset exposure count to 0  "  << "</div>";
+     cout << "<div> Unable to open file  "  << "</div>";
+   }
+
+   }
+
+   
+   
+   
+   myfile.open ("example.html");
 
    //bool isStatus = form.queryCheckbox("status");   // get the state of the status checkbox
    form_iterator it = form.getElement("pwr_cmd");   // get the period text value
@@ -231,22 +252,6 @@ cout <<  "</html>" << endl;
     if (command=="single")
     {
        
-       if (isReset){
-
-  cout << "<div> Resetting the exposure count to 0  "  << "</div>";
-  ofstream myfile2 (ss.str().c_str());
-  if (myfile2.is_open())
-  {
-    myfile2 << 0;
-    myfile2.close();
-  }
-  else {
-     //cout << "Unable to open file";
-     cout << "<div> Unable to reset exposure count to 0  "  << "</div>";
-     cout << "<div> Unable to open file  "  << "</div>";
-   }
-
-   }
 
      i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop");
      cout << "<div> Ran a single exposure ! </div>";
