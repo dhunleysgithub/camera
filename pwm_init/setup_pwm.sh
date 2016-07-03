9f71d1be8ed44fe618de778322f3adc9be7210fe
dhunleysgithub <dwighthunley0@gmail.com>
2016-07-03 09:25:05 -0700

    Update setup_pwm.sh

--- pwm_init/setup_pwm.sh
+++ pwm_init/setup_pwm.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 # PWM assignments script
 
-pwm_directory14="/sys/devices/ocp.3/pwm_test_P9_14.15"
-pwm_directory16="/sys/devices/ocp.3/pwm_test_P9_16.16"
-pwm_directory21="/sys/devices/ocp.3/pwm_test_P9_21.17"
-pwm_directory22="/sys/devices/ocp.3/pwm_test_P9_22.18"
-pwm_directory42="/sys/devices/ocp.3/pwm_test_P9_42.19"
-pwm_directory13="/sys/devices/ocp.3/pwm_test_P8_13.20"
-pwm_directory19="/sys/devices/ocp.3/pwm_test_P8_19.21"
+#pwm_directory14="/sys/devices/ocp.3/pwm_test_P9_14.15"
+#pwm_directory16="/sys/devices/ocp.3/pwm_test_P9_16.16"
+#pwm_directory21="/sys/devices/ocp.3/pwm_test_P9_21.17"
+#pwm_directory22="/sys/devices/ocp.3/pwm_test_P9_22.18"
+#pwm_directory42="/sys/devices/ocp.3/pwm_test_P9_42.19"
+#pwm_directory13="/sys/devices/ocp.3/pwm_test_P8_13.20"
+#pwm_directory19="/sys/devices/ocp.3/pwm_test_P8_19.21"
 
-if [ -d "$pwm_directory14" ] && [ -d "$pwm_directory16" ] && [ -d "$pwm_directory19" ]
-then
-	echo "$pwm_directory14 found."
-	echo "$pwm_directory16 found."
-	echo "$pwm_directory19 found."
-	 if [ -d "$pwm_directory21" ] && [ -d "$pwm_directory22" ]
-	 then
-	 	echo "$pwm_directory21 found."
-	  echo "$pwm_directory22 found."
-    fi
-	    if [ -d "$pwm_directory42" ] && [ -d "$pwm_directory13" ]
-	    then
-	 	   echo "$pwm_directory42 found."
-	     echo "$pwm_directory13 found."
-      fi
+#if [ -d "$pwm_directory14" ] && [ -d "$pwm_directory16" ] && [ -d "$pwm_directory19" ]
+#then
+#	echo "$pwm_directory14 found."
+#	echo "$pwm_directory16 found."
+#	echo "$pwm_directory19 found."
+#	 if [ -d "$pwm_directory21" ] && [ -d "$pwm_directory22" ]
+#	 then
+#	 	echo "$pwm_directory21 found."
+#	  echo "$pwm_directory22 found."
+#    fi
+#	    if [ -d "$pwm_directory42" ] && [ -d "$pwm_directory13" ]
+#	    then
+#	 	   echo "$pwm_directory42 found."
+#	     echo "$pwm_directory13 found."
+#      fi
     
-else
+#else
 
 echo "Initializing PWMs"
 cd /lib/firmware/
@@ -41,4 +41,4 @@ echo am33xx_pwm > $SLOTS
 cat $SLOTS
 ls /sys/devices/ocp.3/
 
-fi
+#fi
