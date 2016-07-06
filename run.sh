22aa50230550a1a2bff5e918c59ac1707f3d1642
dhunleysgithub <dwighthunley0@gmail.com>
2016-07-06 13:53:10 -0700

    added correct paths, commented out program calls, used as for loop test

--- run.sh
+++ run.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
+for ((i=1;i<=$1;i++))
+do
+echo "i= "$i
+done
 
-nice -20 ~/Desktop/prsplttest/prsplt 0
-
-nice -20 ~/Desktop/stepper_plus_tensioner/stepper_plus_tensioner
-
-nice -20 ~/Desktop/prsplttest/prsplt 1
-
-nice -20 ~/Desktop/shutter_openloop/shutter_openloop
-
-nice -20 ~/Desktop/prsplttest/prsplt 0
-
-nice -20 ~/Desktop/stepper_plus_tensioner/stepper_plus_tensioner
-
-nice -20 ~/Desktop/prsplttest/prsplt 1
-
-nice -20 ~/Desktop/shutter_openloop/shutter_openloop
+#nice -20 /root/camera/prsplttest/prsplt 0
+#nice -20 /root/camera/stepper_plus_tensioner/stepper_plus_tensioner
+#nice -20 /root/camera/prsplttest/prsplt 1
+#nice -20 /root/camera/shutter_openloop/shutter_openloop
