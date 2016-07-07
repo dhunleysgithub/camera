c87242651b2ba93861373c8101dfdb44a536ad9a
dhunleysgithub <dwighthunley0@gmail.com>
2016-07-07 14:23:49 -0700

    Update postCamera.cpp

--- cgicc/postCamera.cpp
+++ cgicc/postCamera.cpp
@@ -5,11 +5,11 @@
 #include <cgicc/CgiDefs.h>
 #include <cgicc/HTTPHTMLHeader.h>
 #include <cgicc/HTMLClasses.h>
-#include "LED.h"            // the LED class from Chapter 5 of the book 
 #include <fstream>
 #include <sstream>
 #include <unistd.h> 
 #include <sys/types.h> 
+#include <pthread.h>
 
 using namespace std;
 using namespace cgicc;
@@ -18,6 +18,17 @@ using namespace cgicc;
 //FUNCTION  DECLARATIONS: 
  pid_t popen2(const char *shell_cmd, int *p_fd_in, int *p_fd_out); 
  int str2int(const string &str);
+
+// This is the thread function that will execute when the thread is created
+//  it passes and receives data by void pointers
+void *threadFunction(void *value){
+	int *x = (int *)value;    //cast the data passed to an int pointer
+	while(*x<5){              //while the value of x is less than 5
+		usleep(1000);         //sleep for 1ms - encourage main thread
+		(*x)++;               //increment the value of x by 1
+	}
+	return x;                 //return the pointer x (as a void*)
+}
 
 int main()
 {
@@ -311,6 +322,23 @@ cout <<  "</div>" << endl;
        cout <<  "<div> " << c3 << "</div>";  
      } 
 
+    int x=0, y=0;
+    pthread_t thread;         //this is our handle to the pthread
+    // create the thread, pass the reference, address of the function and data
+    // pthread_create() returns 0 on the successful creation of a thread
+    if(pthread_create(&thread, NULL, &threadFunction, &x)!=0){
+    	cout << "Failed to create the thread" << endl;
+    	return 1;
+    }
+    // at this point the thread was created successfully
+    while(y<5){               // loop and increment y, displaying values
+    	cout << "The value of x=" << x << " and y=" << y++ << endl;
+    	usleep(1000);         // encourage the pthread to run
+    }
+    void* result;             // OPTIONAL: receive data back from pthread
+    pthread_join(thread, &result);   // allow the pthread to complete
+    int *z = (int *) result;         // cast from void* to int* to get z
+    cout << "Final: x=" << x << ", y=" << y << " and z=" << *z << endl;
 
    cout << body() << html();
    
