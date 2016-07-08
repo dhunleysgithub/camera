28cb8c7150719602b48fcbc691a545532182f6ae
dhunleysgithub <dwighthunley0@gmail.com>
2016-07-08 09:20:01 -0700

    Update postCamera.cpp

--- cgicc/postCamera.cpp
+++ cgicc/postCamera.cpp
@@ -46,7 +46,8 @@ void *threadFunction(void *value){
        // i = system("nice -20 /root/camera/prsplttest/prsplt 0");
        // i = system("nice -20 /root/camera/stepper_plus_tensioner/stepper_plus_tensioner");
        // i = system("nice -20 /root/camera/prsplttest/prsplt 1");
-       i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop");
+       //i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop");
+       i = system("/root/camera/shutter_openloop/shutter_openloop");
        }
 
 //        i = system(mcmd_char);
