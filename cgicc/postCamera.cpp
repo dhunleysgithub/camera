47a41be5c9cf8b4d7b8ef4cfdbdeb946073c069d
dhunleysgithub <dwighthunley0@gmail.com>
2016-07-08 14:16:09 -0700

    Update postCamera.cpp

--- cgicc/postCamera.cpp
+++ cgicc/postCamera.cpp
@@ -217,7 +217,7 @@ cout << "</div>";
      //   i = system("nice -20 /root/camera/prsplttest/prsplt 0");
      //   i = system("nice -20 /root/camera/stepper_plus_tensioner/stepper_plus_tensioner");
      //   i = system("nice -20 /root/camera/prsplttest/prsplt 1");
-        i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop");
+        i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop 1");
         logoutput = "Ran a single exposure ";
     }
 
