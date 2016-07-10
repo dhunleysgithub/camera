3378a6b689d4df45e12dd667a41842ee9af9ef17
dhunleysgithub <dwighthunley0@gmail.com>
2016-07-10 13:04:20 -0700

    Update to remove commented out experimental code
    Removed pipe popen2 attempt to stream exposure count update and blocks of code dispersed through out trying to real time update exposure counts while multiple exposures running.  Version prior to this is full of trial and error code that might want to revisit sometimes.

--- cgicc/postCamera.cpp
+++ cgicc/postCamera.cpp
@@ -27,44 +27,22 @@ void *threadFunction(void *value){
    int fd_out3; 
    int fd_in3 =0; 
    pid_t  pid3 = 0;
-	int *x = (int *)value;    //cast the data passed to an int pointer
-	int *number = (int *)value;    //cast the data passed to an int pointer
-        ostringstream temp;  //temp as in temporary
-        temp << *number;
-	while(*x<50){              //while the value of x is less than 5
-		usleep(1000);         //sleep for 1ms - encourage main thread
-		(*x)++;               //increment the value of x by 1
+   int *x = (int *)value;    //cast the data passed to an int pointer
+   int *number = (int *)value;    //cast the data passed to an int pointer
+   ostringstream temp;  //temp as in temporary
+   temp << *number;
+   while(*x<50){              //while the value of x is less than 5
+      usleep(1000);         //sleep for 1ms - encourage main thread
+      (*x)++;               //increment the value of x by 1
 	}
-       exposureCount=temp.str();      //str is temp as string
-     //  runcommandstring = "/usr/lib/cgi-bin/run.sh " + exposureCount + " &";
-       runcommandstring = "/root/camera/shutter_openloop/shutter_openloop " + exposureCount + " &";
-       mcmd_char = runcommandstring.c_str();
-       int i;
-/*
-      for( int a = 1; a < 10 + 1; a = a + 1 )
-       {
-       cout << "<div> Running exposure number " << a << "</div>";
-       // i = system("nice -20 /root/camera/prsplttest/prsplt 0");
-       // i = system("nice -20 /root/camera/stepper_plus_tensioner/stepper_plus_tensioner");
-       // i = system("nice -20 /root/camera/prsplttest/prsplt 1");
-       //i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop");
-       i = system("/root/camera/shutter_openloop/shutter_openloop");
-       }
-*/
-        i = system(mcmd_char);
-/*
-       pid3 = popen2(mcmd_char, &fd_in3, &fd_out3); 
-    while(1)  //read  process  output 
-     { 
-       char c3; 
-       if(read(fd_out3, &c3, 1) <= 0) 
-       break; //no  data 
-       cout <<  "<div> " << c3 << "</div>";  
-     } 
-   */  
-    //   cout <<  "<div> " << runcommandstring << "</div>";  
-       cout << runcommandstring << endl;  
-       return x;                 //return the pointer x ( as a void*)
+   exposureCount=temp.str();      //str is temp as string
+   //  runcommandstring = "/usr/lib/cgi-bin/run.sh " + exposureCount + " &";
+   runcommandstring = "/root/camera/shutter_openloop/shutter_openloop " + exposureCount + " &";
+   mcmd_char = runcommandstring.c_str();
+   int i;
+   i = system(mcmd_char);
+   cout << " Run Command String : " << runcommandstring << endl;  
+   return x;                 //return the pointer x ( as a void*)
 }
 
 int main()
@@ -75,20 +53,17 @@ int number;
 stringstream ss;
 ss << exposureCount_PATH;
 fstream fs;
-
-   int i,j, ierr, iexposuresRequested;
-   ierr=0;
-   iexposuresRequested=0;
+int i,j, ierr, iexposuresRequested;
+ierr=0;
+iexposuresRequested=0;
    
-   Cgicc form;                                     // the CGI form object
+Cgicc form;                                     // the CGI form object
    
-   string pwr_cmd, command, exposureCount, exposuresRequested, exposuresReset, expsReset;
-   string mcmd_str, runcommandstring;
-   runcommandstring = " ";
-   
-   const char* mcmd_char;
-   
-   bool isReset = form.queryCheckbox("expsReset");   // get the state of the status checkbox
+string pwr_cmd, command, exposureCount, exposuresRequested, exposuresReset, expsReset;
+string mcmd_str, runcommandstring;
+runcommandstring = " ";
+const char* mcmd_char;
+bool isReset = form.queryCheckbox("expsReset");   // get the state of the status checkbox
    
    form_iterator it = form.getElement("pwr_cmd");   // get the period text value
    if (it == form.getElements().end() || it->getValue()==""){
@@ -223,25 +198,13 @@ cout << "</div>";
 
     }
 
- //iexposuresRequested = str2int(exposuresRequested);
- // Multiple Exposures Requested
- 
-//   if (command=="multiple")
-//    {
-//     mcmd_str = "/root/camera/run_shutter_ol.sh " + exposuresRequested;
-//     mcmd_char = mcmd_str.c_str();
-//    // i = system(mcmd_char);
-//     i = system("cd /usr/lib/cgi-bin; echo 'Hi Dwight Cur Wdir is ';pwd;'./run_shutter_ol.sh 10' >> ttt; cat ttt;echo damnit > ttt; cat ttt");
-//     logoutput = "Running " + exposuresRequested + " exposures \n using system call argument " + mcmd_str;
-//    }
-    
 // Multiple Exposures Requested
    int fd_out3; 
    int fd_in3 =0; 
    pid_t  pid3 = 0;
    int y=0;
-    int x=0;
-    pthread_t thread;         //this is our handle to the pthread
+   int x=0;
+   pthread_t thread;         //this is our handle to the pthread
 
     if (command=="multiple")
      {
@@ -249,19 +212,7 @@ cout << "</div>";
       runcommandstring = "/usr/lib/cgi-bin/run.sh " + exposuresRequested + " &";
        //mcmd_str = "/root/camera/run_shutter_ol.sh " + exposuresRequested;
        mcmd_char = runcommandstring.c_str();
-     // for( int a = 1; a < iexposuresRequested+1; a = a + 1 )
-     //  {
-        //cout << "<div> Running exposure number " << a << "</div>";
-       // i = system("nice -20 /root/camera/prsplttest/prsplt 0");
-       // i = system("nice -20 /root/camera/stepper_plus_tensioner/stepper_plus_tensioner");
-       // i = system("nice -20 /root/camera/prsplttest/prsplt 1");
-       // i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop");
-    //   }
-       
        logoutput = "Ran " + exposuresRequested + " exposures ";
-
-     //  pid_t  pid3 = popen2("/root/camera/run.sh 100", &fd_in3, &fd_out3); 
-     //  pid3 = popen2("/usr/lib/cgi-bin/run.sh 100", &fd_in3, &fd_out3); 
     // create the thread, pass the reference, address of the function and data
     // pthread_create() returns 0 on the successful creation of a thread
     if(pthread_create(&thread, NULL, &threadFunction, &iexposuresRequested)!=0){
@@ -273,27 +224,13 @@ cout << "</div>";
     	cout << "The value of x=" << x << " and y=" << y++ << endl;
     	sleep(1);         // encourage the pthread to run
     }
+// pthread_join will block execution until thread completes
     void* result;             // OPTIONAL: receive data back from pthread
     //pthread_join(thread, &result);   // allow the pthread to complete
     //int *z = (int *) result;         // cast from void* to int* to get z
     //cout << "Final: x=" << x << ", y=" << y << " and z=" << *z << endl;
    }
-//       pid3 = popen2(mcmd_char, &fd_in3, &fd_out3); 
-/*   
-         if(pid3 <= 0) 
-         { 
-           cout << "<div> Unable to exec prog Test Case 3 </div>"; 
-          } 
-    while(1)  //read  process  output 
-     { 
-       char c3; 
-       if(read(fd_out3, &c3, 1) <= 0) 
-       break; //no  data 
-       cout <<  "<div> " << c3 << "</div>";  
-     } 
-     */
-     
-     
+
 // Read exposure counter value
 fs.open(ss.str().c_str(), fstream::in);
 fs >> number;
@@ -369,15 +306,8 @@ cout <<  "</div>" << endl;
          { 
            cout << "<div> Unable to exec prog Test Case 3 </div>"; 
           } 
-/*
-    while(1)  //read  process  output 
-     { 
-       char c3; 
-       if(read(fd_out3, &c3, 1) <= 0) 
-       break; //no  data 
-       cout <<  "<div> " << c3 << "</div>";  
-     } 
-*/
+
+
 //form.getElement("command")
 //document.getElementById("myTextarea").value = "Fifth Avenue, New York City";
 cout << "<button type=\"button\" onclick=\"myFunction()\">Click me!</button>" << endl;
@@ -389,242 +319,11 @@ cout << "    document.getElementById(\"exposures\").value = \"Fifth Avenue, New 
 cout << "}" << endl;
 cout << "</script>";
 
+cout << body() << html();
 
-   cout << body() << html();
-               cout <<  "DEBUG 1a3" << endl;  
+cout <<  "DEBUG 1a3" << endl;  
 
- /*  
- //**************************************************************************  
-   while(1)
-   {
-               cout <<  "DEBUG 1a4" << endl;  
-
-  // Generate the form but use states that are set in the form that was submitted
-cout << HTTPHTMLHeader() << endl;               // Generate the HTML form using cgicc
-cout << html() << head() << title("EBB C++ Post Camera Baseline") << head() << endl;
-cout << body() << h1("BeagleBone Black Camera Controller") << endl;;
-cout << "<form action=\"/cgi-bin/postCamera.cgi\" method=\"POST\">\n";
-cout <<  "<style>" << endl;
-cout <<  "#header {" << endl;
-cout <<  "    background-color:blue;" << endl;
-cout <<  "    color:white;" << endl;
-cout <<  "    text-align:center;" << endl;
-cout <<  "    padding:5px;" << endl;
-cout <<  "}" << endl;
-cout <<  "#nav {" << endl;
-cout <<  "    line-height:30px;" << endl;
-cout <<  "    background-color:#99ccff;" << endl;
-cout <<  "    height:400px;" << endl;
-cout <<  "    width:300px;" << endl;
-cout <<  "    float:left;" << endl;
-cout <<  "    padding:2px;" << endl;
-cout <<  "}" << endl;
-cout <<  "#nav2 {" << endl;
-cout <<  "    line-height:30px;" << endl;
-cout <<  "    background-color:#ffffcc;" << endl;
-cout <<  "    height:400px;" << endl;
-cout <<  "    width:300px;" << endl;
-cout <<  "    float:left;" << endl;
-cout <<  "    padding:2px;" << endl;
-cout <<  "}" << endl;
-cout <<  "#nav3 {" << endl;
-cout <<  "    line-height:30px;" << endl;
-cout <<  "    background-color:#cc99ff;" << endl;
-cout <<  "    height:400px;" << endl;
-cout <<  "    width:300px;" << endl;
-cout <<  "    float:left;" << endl;
-cout <<  "    padding:2px;" << endl;
-cout <<  "}" << endl;
-cout <<  "#section {" << endl;
-cout <<  "    width:350px;" << endl;
-cout <<  "    float:left;" << endl;
-cout <<  "    padding:10px;" << endl;
-cout <<  "}" << endl;
-cout <<  "#footer {" << endl;
-cout <<  "    background-color:black;" << endl;
-cout <<  "    color:white;" << endl;
-cout <<  "    clear:both;" << endl;
-cout <<  "    text-align:center;" << endl;
-cout <<  "    padding:5px;" << endl;
-cout <<  "}" << endl;
-cout <<  "</style>" << endl;
-cout <<  "</head>" << endl;
-
-cout <<  "<body>" << endl;
-cout <<  "<div id=\"header\">" << endl;
-cout <<  "<h1>Photoengineering Inc.</h1>" << endl;
-cout <<  "</div>" << endl;
-
-cout <<  "<div id=\"nav\">" << endl;
-cout <<  "<h2>Command Selection </h2>" << endl;
-cout << "<input type=\"radio\" name=\"command\" value=\"single\""
-      << ( command=="single" ? "checked":"") << "/> Single <br>";	// is the command="on"?
-cout << "<input type=\"radio\" name=\"command\" value=\"multiple\""
-       << ( command=="multiple" ? "checked":"") << "/> Multiple <br>";
-cout << "<input type=\"radio\" name=\"command\" value=\"load\""
-        << ( command=="load" ? "checked":"") << "/> Load <br>";
-cout << "<input type=\"radio\" name=\"command\" value=\"unload\""
-        << ( command=="unload" ? "checked":"") << "/> Unload <br>";
-cout << "<input type=\"radio\" name=\"command\" value=\"singleadv\""
-        << ( command=="singleadv" ? "checked":"") << "/> Single Adv. <br>";
-cout << "<input type=\"radio\" name=\"command\" value=\"multipleadv\""
-        << ( command=="multipleadv" ? "checked":"") << "/> Multiple Adv. <br>";
-cout << "<input type=\"radio\" name=\"command\" value=\"expreset\""
-        << ( command=="expreset" ? "checked":"") << "/> Reset Exposure Counter <br>";
-cout << "<input type=\"submit\" value=\"Process Selections\" />";
-cout << "</div>";
-
-// Reset Exposure Count Requested
-               cout <<  "DEBUG 2" << endl;  
-
-  if ( command=="expreset"){
-   //  cout << "<div> Resetting the exposure count to 0  "  << "</div>";
-   ofstream myfile2 (ss.str().c_str());
-   if (myfile2.is_open())
-    {
-      myfile2 << 0;
-      myfile2.close();
-      logoutput = "Reset the exposure count to 0";
-   }
-   else {
-    cout << "<div> *** Unable to reset exposure count to 0  "  << "</div>";
-    }
-          }
-
-// Single Exposure Requested
-
-   if (command=="single")
-    {
-     //   i = system("nice -20 /root/camera/prsplttest/prsplt 0");
-     //   i = system("nice -20 /root/camera/stepper_plus_tensioner/stepper_plus_tensioner");
-     //   i = system("nice -20 /root/camera/prsplttest/prsplt 1");
-        i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop 1");
-        logoutput = "Ran a single exposure ";
-    }
-
- //iexposuresRequested = str2int(exposuresRequested);
- // Multiple Exposures Requested
- 
-//   if (command=="multiple")
-//    {
-//     mcmd_str = "/root/camera/run_shutter_ol.sh " + exposuresRequested;
-//     mcmd_char = mcmd_str.c_str();
-//    // i = system(mcmd_char);
-//     i = system("cd /usr/lib/cgi-bin; echo 'Hi Dwight Cur Wdir is ';pwd;'./run_shutter_ol.sh 10' >> ttt; cat ttt;echo damnit > ttt; cat ttt");
-//     logoutput = "Running " + exposuresRequested + " exposures \n using system call argument " + mcmd_str;
-//    }
-    
-// Multiple Exposures Requested
-//   int fd_out3; 
-//   int fd_in3 =0; 
-//   pid_t  pid3 = 0;
-//   int y=0;
-//    int x=0;
-//    pthread_t thread;         //this is our handle to the pthread
-
-    if (command=="multiple")
-     {
-      iexposuresRequested = str2int(exposuresRequested);
-      runcommandstring = "/usr/lib/cgi-bin/run.sh " + exposuresRequested + " &";
-       //mcmd_str = "/root/camera/run_shutter_ol.sh " + exposuresRequested;
-       mcmd_char = runcommandstring.c_str();
-      for( int a = 1; a < iexposuresRequested+1; a = a + 1 )
-       {
-        //cout << "<div> Running exposure number " << a << "</div>";
-       // i = system("nice -20 /root/camera/prsplttest/prsplt 0");
-       // i = system("nice -20 /root/camera/stepper_plus_tensioner/stepper_plus_tensioner");
-       // i = system("nice -20 /root/camera/prsplttest/prsplt 1");
-       // i = system("nice -20 /root/camera/shutter_openloop/shutter_openloop");
-       }
-       
-       logoutput = "Ran " + exposuresRequested + " exposures ";
-
-     //  pid_t  pid3 = popen2("/root/camera/run.sh 100", &fd_in3, &fd_out3); 
-     //  pid3 = popen2("/usr/lib/cgi-bin/run.sh 100", &fd_in3, &fd_out3); 
-    // create the thread, pass the reference, address of the function and data
-    // pthread_create() returns 0 on the successful creation of a thread
-    if(pthread_create(&thread, NULL, &threadFunction, &iexposuresRequested)!=0){
-    	cout << "Failed to create the thread" << endl;
-    	return 1;
-    }
-//       pid3 = popen2(mcmd_char, &fd_in3, &fd_out3); 
-       cout << "<div> popen2 none blocking ?? </div>";
-   
-//         if(pid3 <= 0) 
- //        { 
- //          cout << "<div> Unable to exec prog Test Case 3 </div>"; 
- //         } 
- //   while(1)  //read  process  output 
- //    { 
- //      char c3; 
-  //     if(read(fd_out3, &c3, 1) <= 0) 
-  //     break; //no  data 
-  //     cout <<  "<div> " << c3 << "</div>";  
-   //  } 
-     
-  //   }
-     
-// Read exposure counter value
-fs.open(ss.str().c_str(), fstream::in);
-fs >> number;
-//ostringstream temp;  //temp as in temporary
-temp<<number;
-exposureCount=temp.str();      //str is temp as string
-fs.close();
-
-// 5V Power Control Panel
-
-cout <<  "<div id=\"nav2\">" << endl;
-cout <<  "<h2>Board 5V Power </h2>" << endl;
-cout << "<input type=\"radio\" name=\"pwr_cmd\" value=\"on\""
-      << ( pwr_cmd=="on" ? "checked":"") << "/> On ";	// is the pwr_cmd="on"?
-cout << "<input type=\"radio\" name=\"pwr_cmd\" value=\"off\""
-       << ( pwr_cmd=="off" ? "checked":"") << "/> Off <br><br>";
-cout <<  "<h2>Format </h2>" << endl;
-cout <<  "<select>" << endl;
-cout <<  "  <option value=\"Full\">Full</option>" << endl;
-cout <<  "  <option value=\"Half\">Half</option>" << endl;
-cout <<  "  <option value=\"tbd\">Quarter</option>" << endl;
-cout <<  "</select>" << endl;
-cout <<  "</div>" << endl;
-
-
-cout <<  "<div id=\"nav3\">" << endl;
-cout <<  "<h2>Control </h2>" << endl;
-cout <<  "<label class=\"description\" for=\"element_1\">Exposure Time : </label>" << endl;
-cout <<  "<input id=\"element_1\" name=\"element_1\" class=\"element text small\" type=\"text\" size = \"6\" maxlength=\"25\" value=\"\"/><br>" << endl;
-cout <<  "<label class=\"description\" for=\"element_1\">Multiple Exp. : </label>" << endl;
-cout <<  "<input id=\"element_1\" name=\"multexpreq\" class=\"element text small\" type=\"text\" size = \"6\"  maxlength=\"25\" value=\""<< exposuresRequested << "\"/><br>" << endl;
-cout <<  "<label class=\"description\" for=\"element_1\">Multiple Advance : </label>" << endl;
-cout <<  "<input id=\"element_1\" name=\"element_1\" class=\"element text small\" type=\"text\" size = \"6\"  maxlength=\"5\" value=\"\"/><br>" << endl;
-cout <<  "<label class=\"description\" for=\"element_1\">Exposure Count : </label>" << endl;
-cout <<  "<input id=\"element_1\" name=\"exposures\" class=\"element text small\" type=\"text\" size = \"6\"  maxlength=\"125\" value=\""<< exposureCount << "\"/><br>" << endl;
-cout <<  "<label class=\"description\" for=\"element_1\">Frame Count : </label>" << endl;
-cout <<  "<input id=\"element_1\" name=\"element_1\" class=\"element text small\" type=\"text\" size = \"6\"  maxlength=\"125\" value=\"\"/><br>" << endl;
-cout <<  "<label class=\"description\" for=\"element_1\">Film Used : </label>" << endl;
-cout <<  "<input id=\"element_1\" name=\"element_1\" class=\"element text small\" type=\"text\" size = \"6\"  maxlength=\"125\" value=\"\"/><br>" << endl;
-cout <<  "</div></form>" << endl;
-
-
-
-cout <<  "<div id=\"footer\">" << endl;
-cout <<  "Serial Number: XYZ Phone: 480-406-9804 Email: dwight_hunley@hotmail.com" << endl;
-cout <<  "</div>" << endl;
-               cout <<  "DEBUG 3" << endl;  
-
-sleep(2);
-}
-
-// ****************************************************************** 
- */  
-   
-   
-   
-   
-   
-   
-   
-  return 0;
+return 0;
 
 }
 
